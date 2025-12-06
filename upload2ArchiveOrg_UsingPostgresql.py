b5dbb1ff35651ced411b4c8fdeadaaf53b9d616c
Linkeer365 <564142445@qq.com>
2025-12-06 12:59:11 +0800

    加上了selenium，光用archivenow那种基于requests的还是不太行

--- upload2ArchiveOrg_UsingPostgresql.py
+++ upload2ArchiveOrg_UsingPostgresql.py
@@ -4,9 +4,52 @@ import time
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from archivenow import archivenow
 
 import wayback
+
+def archive_by_selenium(full_url):
+
+    driver.get("https://web.archive.org/save")
+    wait = WebDriverWait(driver, 10)
+    
+    try:
+        input_box = wait.until(EC.presence_of_element_located((By.ID, "web-save-url-input")))
+        input_box.send_keys(full_url)
+    except Exception as e:
+        print(f"填写输入框时出错: {e}")
+    
+    try:
+        checkbox = wait.until(EC.presence_of_element_located((By.ID, "capture_all")))
+        if (checkbox.get_attribute("checked")):
+            checkbox.click()
+    except Exception as e:
+        print(f"操作checkbox时出错: {e}")
+    
+    try:
+        upload_btn = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,"input[type='submit'][value='SAVE PAGE']")))[0]
+        upload_btn.click()
+    except Exception as e:
+        print(f"上传btn获取出错: {e}")
+
+    try:
+        # 1. 显式等待“保存成功”的结果区域出现
+        # 等待id为‘spn-result’的div元素出现在DOM中，最多等待300秒
+        link_element = WebDriverWait(driver, 300).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "#spn-result a"))
+        )
+        generated_link = link_element.get_attribute("href")
+
+    except Exception as e:
+        print(f"保存出错: {e}")
+    
+    return generated_link
 
 def get_already_page(full_url,fresh_date='201701'):
     client = wayback.WaybackClient()
@@ -49,11 +92,16 @@ def get_notyet_archive_link(db_config,field,field_date):
         
         for root,abbr_link,full_url in cursor.fetchall():
             archive_link = get_already_page(full_url)
+            
             if archive_link == None:
-                archive_link = archivenow.push(full_url, "ia")[0]
-                if not archive_link.startswith('https://web.archive.org/web/'):
-                    time.sleep(30)
-                    continue
+                # archive_link = archivenow.push(full_url, "ia")[0]
+                if archive_link == None or not archive_link.startswith('https://web.archive.org/web/'):
+                    time.sleep(10)
+                    archive_link = archive_by_selenium(full_url)
+                    print(archive_link)
+                    if not archive_link.startswith('https://web.archive.org/web/'):
+                        time.sleep(10)
+                        continue
             archive_date = archive_link[date_start_idx:date_end_idx]
             cursor2 = conn.cursor()
             update_table_query = f"""
@@ -69,9 +117,10 @@ def get_notyet_archive_link(db_config,field,field_date):
             with open("u2a.log",'a',encoding="utf-8") as f:
                 f.write(f"{root} : {abbr_link}\n")
                 f.write(archive_link+'\n\n')
-        
+            time.sleep(10)
 
     except Exception as e:
+        print(root,abbr_link,'\t',archive_link)
         print(f"更新到数据库时出错: {e}")
         if conn:
             conn.rollback()
@@ -86,6 +135,8 @@ if __name__ == '__main__':
         'password': 'postgres',
         'port': '5432'
     }
+    
+    driver = webdriver.Chrome()
     cnt = 0
     while cnt<=200:
         get_notyet_archive_link(db_config,'wayback_link1','wayback_date1')
